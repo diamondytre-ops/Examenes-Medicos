470230cb2f2119ab95f24fef7443127e25923c53
diamondytre-ops <diamondytre@gmail.com>
2026-06-02 19:41:35 -0600

    Add files via upload

---
+++ api-config.js
@@ -0,0 +1,15 @@
+/**
+ * api-config.js
+ * -------------
+ * Configuración centralizada de la API del backend.
+ *
+ * INSTRUCCIONES:
+ * 1. Ve a script.google.com y crea un nuevo proyecto.
+ * 2. Pega el contenido de google-apps-script.gs.
+ * 3. Despliega como "Aplicación web":
+ *    - Ejecutar como: Yo (tu cuenta Google)
+ *    - Quién tiene acceso: Cualquier persona
+ * 4. Copia la URL del despliegue y pégala abajo.
+ */
+
+const API_URL = "Uhttps://script.google.com/macros/s/AKfycbzx5h0bfhx0M-mPyRlLtMM5uAn-Exh94H0SrGN67Vcdci7n4Jft1etGLAWkmj3T_BGi/exec";
