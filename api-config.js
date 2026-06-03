dae94688ce04dc28947df3f99aa795387d3bf870
diamondytre-ops <diamondytre@gmail.com>
2026-06-02 21:17:03 -0600

    Update api-config.js

--- api-config.js
+++ api-config.js
@@ -12,4 +12,4 @@
  * 4. Copia la URL del despliegue y pégala abajo.
  */
 
-const API_URL = "https://script.google.com/macros/s/AKfycbyax_wujgoNd-fBNmCouQJyhTgHoKLAk7hm17RG6dRWnq2oQxCm64Nnt1or2TIHcjik/exec";
+const API_URL = "Uhttps://script.google.com/macros/s/AKfycbzx5h0bfhx0M-mPyRlLtMM5uAn-Exh94H0SrGN67Vcdci7n4Jft1etGLAWkmj3T_BGi/exec";
